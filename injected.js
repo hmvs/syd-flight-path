f45f92710ed640367ce0f77e0c343da55c03ec5b
Vadym Kurachevskyi <vadym@flash-fx.com.au>
2025-07-09 23:23:10 +1000

    chore: remove bs comments

--- injected.js
+++ injected.js
@@ -14,17 +14,15 @@ function setupMap() {
     const scriptTag = document.getElementById("flights-syd");
     const geoData = JSON.parse(scriptTag.getAttribute("data-geo-data"));
 
-    // Load GeoJSON data as an object instead of URL
     window.foundMap.data.addGeoJson(geoData);
     window.selectedFlow = "None";
     window.foundMap.data.setStyle(featureStyleFilter);
 
-    // Rest of your existing setup code...
+    //Control div
     const controlDiv = document.createElement('div');
     Object.assign(controlDiv.style, {
         position: 'absolute', top: '10px', right: '100px', background: '#fff', padding: '4px', zIndex: '1'
     });
-
     const select = document.createElement('select');
     ['None', 'East', 'North', 'South', 'West', 'All'].forEach(dir => {
         const opt = document.createElement('option');
@@ -32,10 +30,8 @@ function setupMap() {
         opt.textContent = dir;
         select.appendChild(opt);
     });
-
     controlDiv.appendChild(select);
     window.foundMap.getDiv().appendChild(controlDiv);
-
     select.addEventListener("change", () => {
         const choice = select.value;
         console.log("Selected direction:", choice);
@@ -44,7 +40,6 @@ function setupMap() {
     });
 }
 
-// Rest of your existing findMap function and interval code...
 function findMap(obj, seen = new WeakSet()) {
     if (obj instanceof window.google.maps.Map) {
         return obj;
@@ -70,13 +65,13 @@ function findMap(obj, seen = new WeakSet()) {
     return null;
 }
 
-let attempts = 0;
+let searchMapAttempts = 0;
 const interval = setInterval(() => {
-    if (attempts++ > 60) {
-        console.log("We give up");
+    if (searchMapAttempts++ > 60) {
+        console.log("We give up searching for the map");
         clearInterval(interval);
     }
-    console.log("Looking for map interval!");
+    console.log("Looking for map interval");
 
     try {
         if (window.google?.maps?.Map && window.google.maps.Map instanceof Object) {
